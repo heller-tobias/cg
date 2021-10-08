03859a18a1324d2ced470d1276aedf0041ff09e5
tobiasheller1 <toebi12@gmail.com>
2021-10-08 08:08:31 +0200

    ex3: init

---
+++ ex3/Exercise3.js
@@ -0,0 +1,153 @@
+//
+// Computer Graphics
+//
+// WebGL Exercises
+//
+
+// Register function to call after document has loaded
+window.onload = startup;
+
+// the gl object is saved globally
+var gl;
+
+// we keep all local parameters for the program in a single object
+var ctx = {
+    shaderProgram: -1,
+    aVertexPositionId: -1,
+    uColorId: -1
+    // add local parameters for attributes and uniforms here
+};
+
+// we keep all the parameters for drawing a specific object together
+// Alle Parameter der Objekte!
+var rectangleObject = {
+    buffer: -1,
+    colorBuffer: -1,
+};
+
+var rectangleObject2= {
+    buffer: -1
+};
+
+
+
+/**
+ * Startup function to be called when the body is loaded
+ */
+function startup() {
+    "use strict";
+    var canvas = document.getElementById("myCanvas");
+    gl = createGLContext(canvas);
+    initGL();
+    draw();
+}
+
+/**
+ * InitGL should contain the functionality that needs to be executed only once
+ */
+function initGL() {
+    "use strict";
+    ctx.shaderProgram = loadAndCompileShaders(gl, 'VertexShader.glsl', 'FragmentShader.glsl');
+    setUpAttributesAndUniforms();
+    setUpBuffers();
+
+    // set the clear color here
+    gl.clearColor(0.5,0.5,0.5,1);
+    
+    // add more necessary commands here
+}
+
+/**
+ * Setup all the attribute and uniform variables
+ */
+function setUpAttributesAndUniforms(){
+    "use strict";
+    // add code here to get the ids of attributes and uniform variables from the shaders
+    // Shader Programm wurde oben mit Vertex und Fragment geladen
+    // Linking auf die VertexPositions Variable im Vertexshader!
+    ctx.aVertexPositionId = gl.getAttribLocation(ctx.shaderProgram, "aVertexPosition");
+    ctx.aVertexColorId = gl.getAttribLocation(ctx.shaderProgram, "aVertexColor");
+
+    ctx.uColorId = gl.getUniformLocation(ctx.shaderProgram, "u_color");
+}
+
+function setUpFirstRectangle(){
+    // add code here to setup the buffers for drawing an object
+    //Float 32 Array!
+
+    //Koordinaten von -1 bis +1
+    //Ohne Füllung
+    /*var vertices = [
+        -0.5,-0.5,
+        -0.5, 0.5,
+        0.8,0.5,
+        0.8,-0.5
+    ]*/
+
+    //Mit Fuellung
+    var vertices = [
+        -0.5,-0.5,
+        -0.5, 0.8,
+        0.8,-0.5,
+        0.8,0.8
+    ]
+    rectangleObject.buffer = gl.createBuffer();
+    //Binden des Buffers
+    // Übergeben der Vertexe an den GL Buffer
+    gl.bindBuffer(gl.ARRAY_BUFFER, rectangleObject.buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+
+    rectangleObject.colorBuffer = gl.createBuffer()
+
+    var colors = [
+        1.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+        1.0, 0.0, 0.0, 1.0,
+    ]
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, rectangleObject.colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+}
+
+
+/**
+ * Setup the buffers to use. If more objects are needed this should be split in a file per object.
+ */
+function setUpBuffers(){
+    "use strict";
+    setUpFirstRectangle();
+}
+
+/**
+ * Draw the scene.
+ */
+function draw() {
+    "use strict";
+    console.log("Drawing");
+    //Immer loeschen vor dem Zeichnen!
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    // add drawing routines here
+
+    //Binden des Buffers
+    gl.bindBuffer(gl.ARRAY_BUFFER, rectangleObject.buffer);
+    //Verbindet den aktuellen Buffer mit der Vertexvariable, 2 gibt die Anzahl Komponenten an und float den Datentypen
+    gl.vertexAttribPointer(ctx.aVertexPositionId, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(ctx.aVertexPositionId);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, rectangleObject.colorBuffer);
+    gl.vertexAttribPointer(ctx.aVertexColorId, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(ctx.aVertexColorId);
+
+
+
+    //gl.uniform4fv(ctx.uColorId, [0.0, 1.0, 0.0, 1.0]);
+
+    //Gibt an wie es gezeichnet wird
+    //Ungefuellt
+    //gl.drawArrays(gl.LINE_LOOP, 0, 4);
+
+    //Gefuellt
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+}
---
+++ ex3/VertexShader.glsl
@@ -0,0 +1,11 @@
+//Wird durch Buffer übergeben (vec2 -> Vektor mit Länge 2)
+attribute vec2 aVertexPosition;
+attribute vec4 aVertexColor;
+
+varying vec4 vColor;
+
+void main () {
+    //Muss immer GL Position auf etwas setzen!
+    vColor = aVertexColor;
+    gl_Position = vec4(aVertexPosition, 0, 1);
+}
