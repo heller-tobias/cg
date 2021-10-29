b32a2ad5c1ec5cc46ade29fe321057c87c2b6f5c
tobiasheller1 <toebi12@gmail.com>
2021-10-29 08:54:05 +0200

    ex 5: init

---
+++ ex5/FragmentShader.glsl
@@ -0,0 +1,14 @@
+//Berechnungsgenauigkeit
+precision mediump float;
+
+varying vec4 vColor;
+
+// For uniform color, not used anymore!
+uniform vec4 u_color;
+
+
+//Berechnung der Pixelfarbe
+void main() {
+     // For setting from varying
+     gl_FragColor = vec4(1.0,1.0,1.0,1);
+}
