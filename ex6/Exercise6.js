33c1e1f766b9231a9d75919f4a5319aff43283ed
tobiasheller1 <toebi12@gmail.com>
2021-11-12 09:35:44 +0100

    ex6: init exercise

---
+++ ex6/Exercise6.js
@@ -0,0 +1,207 @@
+//
+// Computer Graphics
+//
+// WebGL Exercises
+//
+
+// Register function to call after document has loaded
+window.onload = startup;
+
+// the gl object is saved globally
+var gl;
+
+var filledCube;
+
+// we keep all local parameters for the program in a single object
+var ctx = {
+    shaderProgram: -1,
+    aVertexPositionId: -1,
+    aVertexColorId: -1,
+    aTextureCoordId: -1,
+    aVertexNormalId: -1,
+    uProjectionMatId: -1,
+    uModelViewMat: -1,
+    uNormalMat: -1,
+    uSampler: -1,
+    uEnableTexture: -1
+    // add local parameters for attributes and uniforms here
+};
+
+var lastUpdate = -1;
+
+/**
+ * Startup function to be called when the body is loaded
+ */
+function startup() {
+    "use strict";
+    var canvas = document.getElementById("myCanvas");
+    gl = createGLContext(canvas);
+    initGL();
+    window.requestAnimationFrame(drawAnimated);
+}
+
+/**
+ * InitGL should contain the functionality that needs to be executed only once
+ */
+function initGL() {
+    "use strict";
+    ctx.shaderProgram = loadAndCompileShaders(gl, 'VertexShader.glsl', 'FragmentShader.glsl');
+    setUpBackfaceCulling();
+    setUpZBuffer();
+    setUpAttributesAndUniforms();
+    setUpBuffers();
+    setUpFilledCube();
+    // set the clear color here
+    gl.clearColor(0.0,0.0,0.0,1);
+}
+
+/**
+ * Setup all the attribute and uniform variables
+ */
+function setUpAttributesAndUniforms(){
+    "use strict";
+    // add code here to get the ids of attributes and uniform variables from the shaders
+    // Shader Programm wurde oben mit Vertex und Fragment geladen
+    // Linking auf die VertexPositions Variable im Vertexshader!
+    ctx.aVertexPositionId = gl.getAttribLocation(ctx.shaderProgram, "aVertexPosition");
+    ctx.aVertexColorId = gl.getAttribLocation(ctx.shaderProgram, "aVertexColor");
+    ctx.aTextureCoordId = gl.getAttribLocation(ctx.shaderProgram, "aVertexTextureCoord");
+    ctx.aVertexNormalId = gl.getAttribLocation(ctx.shaderProgram, "aVertexNormal");
+
+    ctx.uProjectionMatId = gl.getUniformLocation(ctx.shaderProgram, "uProjectionMatrix");
+    ctx.uModelViewMat = gl.getUniformLocation(ctx.shaderProgram, "uModelViewMatrix");
+    ctx.uNormalMat = gl.getUniformLocation(ctx.shaderProgram, "uNormalMatrix");
+    ctx.uSampler = gl.getUniformLocation(ctx.shaderProgram, "uSampler");
+    ctx.uEnableTexture = gl.getUniformLocation(ctx.shaderProgram, "uEnableTexture");
+}
+
+/**
+ * Setup the buffers to use. If more objects are needed this should be split in a file per object.
+ */
+function setUpBuffers(){
+    "use strict";
+    setupProjectionMatrix();
+}
+
+function setUpFilledCube() {
+    filledCube =  FilledCube(gl, [[0,0,1,1.0],[1,0,0,1.0],[0,1,0,1.0],[1,1,0,1.0],[1,0,1,1.0],[0.4,0.4,0.4,1.0]], "lena512.png",);
+}
+
+/**
+ * SetUp Backface Culling.
+ */
+function setUpBackfaceCulling() {
+    gl.frontFace (gl.CCW ) ; // defines how the front face is drawn -> CCW -> Counter Clock Wise!
+    gl.cullFace (gl.BACK ) ; // defines which face should be culled -> Die rückwärtigen Flächen weglassen
+    gl.enable (gl.CULL_FACE ) ; // enables culling
+}
+
+/**
+ * SetUp ZBuffer Algorithm.
+ */
+function setUpZBuffer(){
+    gl.enable (gl.DEPTH_TEST) ;
+}
+
+function setUpModel(timestamp, position, viewMat){
+    mat4.translate(viewMat, viewMat, vec3.fromValues(position[0], position[1], position[2]));
+    mat4.rotate(viewMat, viewMat, timestamp * 0.001, [0.0,0.0,1.0]);
+
+    gl.uniformMatrix4fv ( ctx.uModelViewMat , false , viewMat ) ;
+
+    var normalMatrix = mat3.create()
+    mat3.normalFromMat4 (normalMatrix, viewMat) ;
+    gl . uniformMatrix3fv (ctx.uNormalMatrixId , false , normalMatrix ) ;
+}
+
+function setupProjectionMatrix(){
+    // Set up the world coordinates
+    var projectionMat = mat4.create () ;
+    mat4.perspective(projectionMat, 1.8, gl.drawingBufferWidth/gl.drawingBufferHeight, 0.01, 10)
+    gl.uniformMatrix4fv ( ctx.uProjectionMatId , false , projectionMat ) ;
+}
+
+function setupModelViewFront(timestamp, position){
+    var viewMat = mat4.create () ;
+    mat4.lookAt(viewMat, [0,-2,0.0], [0.0,0,0.0], [0,0,1]);
+    setUpModel(timestamp, position, viewMat);
+}
+
+function setupModelViewTop(timestamp, position){
+    var viewMat = mat4.create () ;
+    mat4.lookAt(viewMat, [0,0,2], [0,0,0], [0,1,0]);
+    setUpModel(timestamp, position, viewMat);
+}
+
+function setupModelViewLeft(timestamp, position){
+    var viewMat = mat4.create () ;
+    mat4.lookAt(viewMat, [-3,0,1], [0,0,0], [0,0,1]);
+    setUpModel(timestamp, position, viewMat);
+}
+
+function setupModelViewRight(timestamp, position){
+    var viewMat = mat4.create () ;
+    mat4.lookAt(viewMat, [3,0,1], [0,0,0], [0,0,1]);
+    setUpModel(timestamp, position, viewMat);
+}
+
+function drawAnimated(timestamp){
+    // calculate time since last call
+    draw(timestamp);
+    // request the next frame
+    window.requestAnimationFrame ( drawAnimated ) ;
+}
+
+function drawCubes(timestamp = 0, modelView){
+    modelView(timestamp, [1.0,0.0,0]);
+    filledCube.drawWithTexture(gl, ctx.aVertexPositionId, ctx.aVertexColorId, ctx.uEnableTexture, ctx.uSampler, ctx.aTextureCoordId);
+
+    modelView(timestamp, [-1.0,0.0,0]);
+    filledCube.draw(gl, ctx.aVertexPositionId, ctx.aVertexColorId, ctx.uEnableTexture, ctx.uSampler, ctx.aTextureCoordId);
+}
+
+function drawCubesFrom4Perspectives(timestamp){
+    gl.viewport(0, 0, gl.canvas.width / 2, gl.canvas.height / 2);
+    drawCubes(timestamp, setupModelViewLeft);
+
+    gl.viewport(gl.canvas.width / 2, 0, gl.canvas.width / 2, gl.canvas.height / 2);
+    drawCubes(timestamp, setupModelViewRight);
+
+    gl.viewport(0, gl.canvas.height / 2, gl.canvas.width / 2, gl.canvas.height / 2);
+    drawCubes(timestamp, setupModelViewTop);
+
+    gl.viewport(gl.canvas.width / 2, gl.canvas.height / 2, gl.canvas.width / 2, gl.canvas.height / 2);
+    drawCubes(timestamp, setupModelViewFront);
+}
+
+/**
+ * Draw the scene.
+ */
+function draw(timestamp = 0) {
+    console.log("Drawing");
+    //Immer loeschen vor dem Zeichnen!
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+    drawCubesFrom4Perspectives(timestamp);
+}
+
+// Key Handling
+var key = {
+    _pressed: {},
+
+    LEFT: 37,
+    UP: 38,
+    RIGHT: 39,
+    DOWN: 40
+};
+
+function isDown (keyCode) {
+    return key._pressed[keyCode];
+}
+
+function onKeydown(event) {
+    key._pressed[event.keyCode] = true;
+}
+
+function onKeyup(event) {
+    delete key._pressed[event.keyCode];
+}
---
+++ ex6/filledCube.js
@@ -0,0 +1,219 @@
+/**
+ *
+ * Define a filled cube with methods for drawing it.
+ *
+ * @param gl the webgl context
+ * @param colors the colors of the sides
+ * @param textureUrl a URL of the texture if available.
+ * @returns object with draw method
+ * @constructor
+ */
+function FilledCube ( gl , colors, textureUrl = "" ) {
+    function defineVertices ( gl ) {
+// define the vertices of the cube
+        var vertices = [
+            //Side 0
+            -0.5,-0.5,-0.5, //0
+            0.5,-0.5,-0.5,  //1
+            0.5,-0.5,0.5,   //2
+            -0.5,-0.5,0.5,  //3
+            //Side 1
+            0.5,-0.5,-0.5,  //4
+            0.5,0.5,-0.5,  //5
+            0.5,0.5,0.5,   //6
+            0.5,-0.5,0.5,  //7
+            //Side 2
+            0.5,-0.5,0.5,   //8
+            0.5,0.5,0.5,    //9
+            -0.5,0.5,0.5,  //10
+            -0.5,-0.5,0.5,  //11
+            //Side 3
+            -0.5,0.5,-0.5,  //12
+            -0.5,0.5,0.5,   //13
+            0.5,0.5,0.5,   //14
+            0.5,0.5,-0.5,  //15
+            //Side 4
+            -0.5,-0.5,-0.5, //16
+            -0.5,0.5,-0.5,  //17
+            0.5,0.5,-0.5,  //18
+            0.5,-0.5,-0.5,  //19
+            //Side 5
+            -0.5,-0.5,-0.5, //20
+            -0.5,-0.5,0.5,   //21
+            -0.5,0.5,0.5,   //22
+            -0.5,0.5,-0.5   //23
+        ];
+        var buffer = gl.createBuffer () ;
+        gl.bindBuffer ( gl.ARRAY_BUFFER , buffer ) ;
+        gl.bufferData ( gl.ARRAY_BUFFER , new Float32Array (vertices) , gl.STATIC_DRAW ) ;
+        return buffer;
+    }
+    function defineTriangles ( gl ) {
+        // define the triangles for the cube , there are 12 triangles in a cube
+        var vertexIndices = [
+            0,1,2,
+            2,3,0,
+            4,5,6,
+            6,7,4,
+            8,9,10,
+            10,11,8,
+            12,13,14,
+            14,15,12,
+            16,17,18,
+            18,19,16,
+            20,21,22,
+            22,23,20
+        ];
+        var buffer = gl.createBuffer () ;
+        gl.bindBuffer ( gl.ELEMENT_ARRAY_BUFFER , buffer ) ;
+        gl.bufferData ( gl.ELEMENT_ARRAY_BUFFER , new Uint16Array (vertexIndices) , gl.STATIC_DRAW ) ;
+        return buffer;
+    }
+
+    function defineColors ( gl ) {
+        var colorIndices = [
+            colors[0][0], colors[0][1], colors[0][2],  colors[0][3],
+            colors[0][0], colors[0][1], colors[0][2],  colors[0][3],
+            colors[0][0], colors[0][1], colors[0][2],  colors[0][3],
+            colors[0][0], colors[0][1], colors[0][2],  colors[0][3],
+            colors[1][0], colors[1][1], colors[1][2],  colors[1][3],
+            colors[1][0], colors[1][1], colors[1][2],  colors[1][3],
+            colors[1][0], colors[1][1], colors[1][2],  colors[1][3],
+            colors[1][0], colors[1][1], colors[1][2],  colors[1][3],
+            colors[2][0], colors[2][1], colors[2][2],  colors[2][3],
+            colors[2][0], colors[2][1], colors[2][2],  colors[2][3],
+            colors[2][0], colors[2][1], colors[2][2],  colors[2][3],
+            colors[2][0], colors[2][1], colors[2][2],  colors[2][3],
+            colors[3][0], colors[3][1], colors[3][2],  colors[3][3],
+            colors[3][0], colors[3][1], colors[3][2],  colors[3][3],
+            colors[3][0], colors[3][1], colors[3][2],  colors[3][3],
+            colors[3][0], colors[3][1], colors[3][2],  colors[3][3],
+            colors[4][0], colors[4][1], colors[4][2],  colors[4][3],
+            colors[4][0], colors[4][1], colors[4][2],  colors[4][3],
+            colors[4][0], colors[4][1], colors[4][2],  colors[4][3],
+            colors[4][0], colors[4][1], colors[4][2],  colors[4][3],
+            colors[5][0], colors[5][1], colors[5][2],  colors[5][3],
+            colors[5][0], colors[5][1], colors[5][2],  colors[5][3],
+            colors[5][0], colors[5][1], colors[5][2],  colors[5][3],
+            colors[5][0], colors[5][1], colors[5][2],  colors[5][3],
+        ];
+        var buffer = gl.createBuffer () ;
+        gl.bindBuffer ( gl.ARRAY_BUFFER , buffer ) ;
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colorIndices), gl.STATIC_DRAW);
+        return buffer;
+    }
+
+    function defineTextureCoord(gl){
+        var textureCoord = [
+            1.0, 0.0,
+            0.0, 0.0,
+            0.0, 1.0,
+            1.0, 1.0,
+            1.0, 0.0,
+            0.0, 0.0,
+            0.0, 1.0,
+            1.0, 1.0,
+            1.0, 0.0,
+            0.0, 0.0,
+            0.0, 1.0,
+            1.0, 1.0,
+            //Side 3
+            1.0, 0.0,
+            1.0, 1.0,
+            0.0, 1.0,
+            0.0, 0.0,
+            //Side 4
+            1.0, 0.0,
+            0.0, 0.0,
+            0.0, 1.0,
+            1.0, 1.0,
+            //Side 5
+            1.0, 0.0,
+            1.0, 1.0,
+            0.0, 1.0,
+            0.0, 0.0,
+        ]
+
+        var buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoord), gl.STATIC_DRAW);
+        return buffer;
+    }
+
+    function initTexture(image, textureObject) {
+        // create a new texture
+        gl.bindTexture(gl.TEXTURE_2D, textureObject);
+
+        // set parameters for the texture
+        gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+        gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+        // Was passiert beim skalieren?
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
+        gl.generateMipmap(gl.TEXTURE_2D);
+
+        //turn texture off again
+        gl.bindTexture(gl.TEXTURE_2D, null);
+    }
+    function defineTexture(gl) {
+        if(textureUrl === ""){
+            console.log("Texture not available")
+            return;
+        }
+        var image = new Image();
+        // create a texture Object
+        var textureObj = gl.createTexture();
+        image.onload = function(){
+            initTexture(image, textureObj);
+            // redraw after loading the texture
+            draw();
+        }
+        // Setting of the image
+        image.src = textureUrl;
+        return textureObj;
+    }
+
+    return {
+        bufferVertices : defineVertices ( gl ) ,
+        bufferTriangles : defineTriangles ( gl ) ,
+        bufferColors: defineColors(gl),
+        textureCoord: defineTextureCoord(gl),
+        textureObj: defineTexture(gl),
+
+        draw : function ( gl , aVertexPositionId , aVertexColorId, uEnableTexture) {
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.bufferVertices);
+            gl.vertexAttribPointer(aVertexPositionId, 3, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(aVertexPositionId);
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.bufferColors);
+            gl.vertexAttribPointer(aVertexColorId, 4, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(aVertexColorId);
+
+            gl.uniform1i(uEnableTexture, 0);
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.bufferTriangles);
+            gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
+        },
+        drawWithTexture(gl, aVertexPositionId , aVertexColorId, uEnableTexture, aSampler, aTextureCoord){
+            gl.uniform1i(uEnableTexture, 1);
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.textureCoord);
+            gl.vertexAttribPointer(aTextureCoord, 2, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(aTextureCoord);
+
+            gl.activeTexture(gl.TEXTURE0);
+            gl.bindTexture(gl.TEXTURE_2D, this.textureObj);
+            gl.uniform1i(aSampler, 0);
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.bufferVertices);
+            gl.vertexAttribPointer(aVertexPositionId, 3, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(aVertexPositionId);
+
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.bufferColors);
+            gl.vertexAttribPointer(aVertexColorId, 4, gl.FLOAT, false, 0, 0);
+            gl.enableVertexAttribArray(aVertexColorId);
+
+            gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.bufferTriangles);
+            gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
+        }
+    }
+}
